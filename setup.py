80bd3a41966400981d4ce6f5e3d80b4c951a7b43
Kit <kitbeyer@gmail.com>
2021-04-15 11:12:06 -0400

    Change version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nwpipeline",
-    version="0.0.1",
+    version="0.0.0",
     author="Kit Beyer",
     author_email="kitbeyer@gmail.com",
     description="NiMBaLWear wearable data processing pipeline.",
