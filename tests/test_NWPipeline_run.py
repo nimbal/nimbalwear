2bcab88377e07c48e36a54c5c5dd37326b3ecc98
Kit <kitbeyer@gmail.com>
2021-04-15 22:39:04 -0400

    testing

--- tests/test_NWPipeline_run.py
+++ tests/test_NWPipeline_run.py
@@ -1,8 +1,8 @@
 import nwpipeline as nwpl
 
-study_dir = '/Volumes/KIT_DATA/test_study'
+study_dir = '/Volumes/KIT_DATA/test_PD_DANCE'
 
-subject_ids = ['8600']
+subject_ids = ['0001']
 coll_ids = ['01']
 
 test_nwpl = nwpl.NWPipeline(study_dir)
