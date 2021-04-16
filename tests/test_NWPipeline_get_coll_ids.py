b8c7eee3622fa72ee5dd24fa4dae1aba7ac7cf84
Kit <kitbeyer@gmail.com>
2021-04-15 20:51:02 -0400

    testing

--- tests/test_NWPipeline_get_coll_ids.py
+++ tests/test_NWPipeline_get_coll_ids.py
@@ -3,6 +3,6 @@ import nwpipeline as nwpl
 study_dir = '/Volumes/KIT_DATA/test_study'
 
 test_nwpl = nwpl.NWPipeline(study_dir)
-subject_ids = test_nwpl.get_subject_ids()
+coll_ids = test_nwpl.get_coll_ids()
 
-print(subject_ids)
+print(coll_ids)
