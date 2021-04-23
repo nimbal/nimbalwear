4a76faeae86ca0d5974c614824779b0cbd82e005
Kit <kitbeyer@gmail.com>
2021-04-22 22:11:06 -0400

    fix __version__

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 import setuptools
+import re
+
+def get_property(prop, project):
+    result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop), open(project + '/__init__.py').read())
+    return result.group(1)
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+project_name = "nwpipeline"
+
 setuptools.setup(
-    name="nwpipeline",
-    version="0.2.0",
+    name=project_name,
+    version=get_property('__version__', project_name),
     author="Kit Beyer",
     author_email="kitbeyer@gmail.com",
     description="NiMBaLWear wearable data processing pipeline.",
