968f5704420e9a45c3975405f222f3261a8fa164
Kit <kitbeyer@gmail.com>
2021-07-14 13:34:36 -0400

    testing

--- tests/test_NWPipeline_run.py
+++ tests/test_NWPipeline_run.py
@@ -1,16 +1,16 @@
 import nwpipeline as nwpl
 
-study_dir = r'W:\NiMBaLWEAR\OND08'
+study_dir = r'W:\NiMBaLWEAR\test-HANDDS'
 
 test_nwpl = nwpl.NWPipeline(study_dir)
 
 #subject_ids = test_nwpl.get_subject_ids()
 #coll_ids = test_nwpl.get_coll_ids()
 
-subject_ids = ['0004', '0007', '0009', '0012']
+subject_ids = ['061621']
 #coll_ids = ['01']
 
 # print(subject_ids)
 # print(coll_ids)
 
-test_nwpl.run(subject_ids=subject_ids, single_stage='sleep', overwrite_header=True, gait_axis=0, quiet=True, log=True)
+test_nwpl.run(subject_ids=subject_ids, single_stage='gait', overwrite_header=True, gait_axis=0, quiet=True, log=True)
--- tests/test_plot_accel.py
+++ tests/test_plot_accel.py
@@ -11,12 +11,12 @@ import matplotlib.pyplot as plt
 
 study_dir = os.path.abspath(r'W:\NiMBaLWEAR\test-HANDDS')
 
-device_edf = os.path.join(study_dir, r'processed\standard_device_edf\AXV6\test-HANDDS_061621_01_AXV6_RWrist.edf')
+device_edf = os.path.join(study_dir, r'processed\standard_device_edf\AXV6\test-HANDDS_061621_01_AXV6_RAnkle.edf')
 
-nonwear_csv = os.path.join(study_dir, r'analyzed\nonwear\standard_nonwear_times\AXV6\test-HANDDS_061621_01_AXV6_RWrist_NONWEAR.csv')
+nonwear_csv = os.path.join(study_dir, r'analyzed\nonwear\standard_nonwear_times\AXV6\test-HANDDS_061621_01_AXV6_RAnkle_NONWEAR.csv')
 gait_csv = os.path.join(study_dir, r'analyzed\gait\gait_bouts\test-HANDDS_061621_01_GAIT_BOUTS.csv')
-#sptw_csv = os.path.join(study_dir, r'analyzed\sleep\sptw\test-HANDDS_061621_01_SPTW.csv')
-#sleep_csv = os.path.join(study_dir, r'analyzed\sleep\sleep_bouts\test-HANDDS_061621_01_SLEEP_BOUTS.csv')
+sptw_csv = os.path.join(study_dir, r'analyzed\sleep\sptw\test-HANDDS_061621_01_SPTW.csv')
+sleep_csv = os.path.join(study_dir, r'analyzed\sleep\sleep_bouts\test-HANDDS_061621_01_SLEEP_BOUTS.csv')
 
 # test_ReMiNDD
 
@@ -33,25 +33,21 @@ print("Reading event files")
 
 # import nonwear
 nonwear = pd.read_csv(nonwear_csv, dtype=str)
-
 nonwear['start_time'] = pd.to_datetime(nonwear['start_time'], format='%Y-%m-%d %H:%M:%S')
 nonwear['end_time'] = pd.to_datetime(nonwear['end_time'], format='%Y-%m-%d %H:%M:%S')
 
-# # import sptw
-# sptw = pd.read_csv(sptw_csv, dtype=str)
-#
-# sptw['start_time'] = pd.to_datetime(sptw['start_time'], format='%Y-%m-%d %H:%M:%S')
-# sptw['end_time'] = pd.to_datetime(sptw['end_time'], format='%Y-%m-%d %H:%M:%S')
-#
-# # import sleep
-# sleep_bouts = pd.read_csv(sleep_csv, dtype=str)
-#
-# sleep_bouts['start_time'] = pd.to_datetime(sleep_bouts['start_time'], format='%Y-%m-%d %H:%M:%S')
-# sleep_bouts['end_time'] = pd.to_datetime(sleep_bouts['end_time'], format='%Y-%m-%d %H:%M:%S')
+# import sptw
+sptw = pd.read_csv(sptw_csv, dtype=str)
+sptw['start_time'] = pd.to_datetime(sptw['start_time'], format='%Y-%m-%d %H:%M:%S')
+sptw['end_time'] = pd.to_datetime(sptw['end_time'], format='%Y-%m-%d %H:%M:%S')
+
+# import sleep
+sleep_bouts = pd.read_csv(sleep_csv, dtype=str)
+sleep_bouts['start_time'] = pd.to_datetime(sleep_bouts['start_time'], format='%Y-%m-%d %H:%M:%S')
+sleep_bouts['end_time'] = pd.to_datetime(sleep_bouts['end_time'], format='%Y-%m-%d %H:%M:%S')
 
 # import gait
 gait_bouts = pd.read_csv(gait_csv, dtype=str)
-
 gait_bouts['start_time'] = pd.to_datetime(gait_bouts['start_timestamp'], format='%Y-%m-%d %H:%M:%S')
 gait_bouts['end_time'] = pd.to_datetime(gait_bouts['end_timestamp'], format='%Y-%m-%d %H:%M:%S')
 
@@ -67,8 +63,8 @@ print("Wrangling")
 
 start_datetime = nwdevice.header['startdate']
 
-start_time = start_datetime+ dt.timedelta(hours=0)
-end_time = start_datetime + dt.timedelta(hours=27)
+start_time = start_datetime+ dt.timedelta(hours=27)
+end_time = start_datetime + dt.timedelta(hours=39)
 
 nwdevice.crop(new_start_time=start_time, new_end_time=end_time)
 
@@ -102,11 +98,11 @@ print("Plotting events")
 for index, row in nonwear.iterrows():
     graph.axvspan(xmin=row['start_time'], xmax=row['end_time'], ymin=0, ymax=0.01, alpha=0.5, color='red')
 
-# for index, row in sptw.iterrows():
-#     graph.axvspan(xmin=row['start_time'], xmax=row['end_time'], ymin=0.015, ymax=0.025, alpha=0.5, color='grey')
-#
-# for index, row in sleep_bouts.iterrows():
-#     graph.axvspan(xmin=row['start_time'], xmax=row['end_time'], ymin=0.03, ymax=0.04, alpha=0.5, color='cyan')
+for index, row in sptw.iterrows():
+    graph.axvspan(xmin=row['start_time'], xmax=row['end_time'], ymin=0.015, ymax=0.025, alpha=0.5, color='grey')
+
+for index, row in sleep_bouts.iterrows():
+    graph.axvspan(xmin=row['start_time'], xmax=row['end_time'], ymin=0.03, ymax=0.04, alpha=0.5, color='cyan')
 
 for index, row in gait_bouts.iterrows():
     graph.axvspan(xmin=row['start_time'], xmax=row['end_time'], ymin=0.045, ymax=0.055, alpha=0.5, color='green')
