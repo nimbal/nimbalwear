5f2a0cc579d75f6d5fc99a489abaa1c0fcc6bd45
Kit <kitbeyer@gmail.com>
2021-07-14 22:18:39 -0400

    fix sleep log entry

--- tests/test_NWPipeline_run.py
+++ tests/test_NWPipeline_run.py
@@ -7,10 +7,10 @@ test_nwpl = nwpl.NWPipeline(study_dir)
 #subject_ids = test_nwpl.get_subject_ids()
 #coll_ids = test_nwpl.get_coll_ids()
 
-subject_ids = ['061621']
+#subject_ids = ['061621']
 #coll_ids = ['01']
 
 # print(subject_ids)
 # print(coll_ids)
 
-test_nwpl.run(subject_ids=subject_ids, single_stage='gait', overwrite_header=True, gait_axis=0, quiet=True, log=True)
+test_nwpl.run(single_stage='sleep', overwrite_header=True, gait_axis=0, quiet=True, log=True)
