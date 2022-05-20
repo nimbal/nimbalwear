27eb73c14e1fc0718ff2ba6ae839ad8ebfc079b0
Kit <kitbeyer@gmail.com>
2022-05-19 21:31:33 -0400

    add script

---
+++ scripts/run_pipeline.py
@@ -0,0 +1,12 @@
+from src.nimbalwear import Pipeline
+
+study_dir = 'w:/NiMBaLWEAR/OND09'
+
+collections = [('0095', '01')]
+#collections = None
+single_stage = None
+
+ond09 = Pipeline(study_dir)
+
+
+ond09.run(collections=collections, single_stage=single_stage, quiet=True, log=True)
